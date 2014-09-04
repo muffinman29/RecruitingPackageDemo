5c1d2bd1b286f40e176db4ac48c3128b0bf0ac03
Steven Williams <steven.williams12083@gmail.com>
2014-09-04 12:14:13 -0500

    Initial Commit

---
+++ RecruitingPackageDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecruitingPackageDemo.Startup))]
+namespace RecruitingPackageDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
